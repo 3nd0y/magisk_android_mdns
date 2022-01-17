fe9c0399eac084973dc41ac78c65153bc49e86cc
3nd0y <fadli.m.m@gmail.com>
2022-01-17 23:01:14 +0900

    change structure

---
+++ service.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+
+while ! [ `pgrep -x mini_mdns` ] ; do
+	$MODDIR/system/bin/mini_dns && sleep 15;
+done
